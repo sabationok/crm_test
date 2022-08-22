9814803f3339454ee9758b5c8b0cbdc5ff47de65
goodcat46 <110983329+sabationok@users.noreply.github.com>
2022-08-22 14:05:12 +0300

    1.012
    + slider fix
    + card - start dev

--- src/slick.js
+++ src/slick.js
@@ -1,21 +1,22 @@
-// ================================ SLIDER__TEST_1
-/*$(document).ready(function () {
+// ================================ SLIDER__BRANDS
+$(document).ready(function () {
   // тут вказується клас DIVчика яз якого потрібно зробити слайдер
-  $('.product-cards_slider').slick({
+  $('.slick__brands-slider').slick({
     dots: true,
+    // dots: false,
     infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    // centerMode: true,
-    // rtl: true,
-    responsive: [
+    // slidesToShow: 1,
+    // slidesToScroll: 1,
+    arrows: false,
+
+    /*responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 1100,
         settings: {
           arrows: false,
           centerMode: true,
           centerPadding: '40px',
-          slidesToShow: 3
+          slidesToShow: 1
         }
       },
       {
@@ -27,9 +28,9 @@
           slidesToShow: 1
         }
       }
-    ]
+    ]*/
 
     // prevArrow: '<button type="button" class="slick-prev">&#60;</button>',
     // nextArrow: '<button type="button" class="slick-next">&#62;</button>',
   });
-});*/
+});
